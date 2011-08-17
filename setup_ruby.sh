8cd11a2a64f056d1bde1ca07015629e2ef807dc1
Matthew Rothenberg <mrothenberg@gmail.com>
2011-08-17 14:17:08 -0300

    fixed re-sourcing profile env mid-script

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
-hash -r #TODO: this doesnt properly reload, need to fix...
+#hash -r
+source ~/.bash_profile
 rvm install 1.9.2
 rvm use ruby-1.9.2
 gem install rails bundler 
