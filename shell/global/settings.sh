2dc80d214ab0bc110828eeea7adb700cbaaa7598
Matthew Rothenberg <mrothenberg@gmail.com>
2012-04-16 19:28:52 -0400

    make vim default git editor

--- shell/global/settings.sh
+++ shell/global/settings.sh
@@ -1,3 +1,5 @@
 #
 # global environment settings/variables
 #
+export GIT_EDITOR='vim'
+
--- shell/os/darwin.sh
+++ shell/os/darwin.sh
@@ -2,10 +2,6 @@
 # MacOSX specific setup for bash
 #
 
-# use textmate
-#export BUNDLER_EDITOR=mate
-#export GIT_EDITOR='vim'
-
 # go to open finder window in terminal
 # http://hints.macworld.com/article.php?story=20110817133623734
 alias cdf='cd `osascript -e "tell application \"Finder\" to if window 1 exists then if target of window 1 as string is not \":\" then get POSIX path of (target of window 1 as alias)"`'
