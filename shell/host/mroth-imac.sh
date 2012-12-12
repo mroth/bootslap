20fee03dad576d0ea458be345928ff9ee021fa22
Matthew Rothenberg <mrothenberg@gmail.com>
2012-12-12 15:50:26 -0500

    remove now deprecated shell files stuff

--- shell/host/mroth-imac.sh
+++
@@ -1,7 +0,0 @@
-### host-specific configurations
-
-#aws shit
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
-export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
